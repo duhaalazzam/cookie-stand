a92a11499a9705e6a5aa4ddd23aa4a3b883f62f9
duhaalazzam <dohabalazzam@gmail.com>
2021-02-15 20:28:28 +0200

    class07

--- js/app.js
+++ js/app.js
@@ -1,203 +1,94 @@
+/* eslint-disable no-trailing-spaces */
 'use strict';
 let timeArr=['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
-let seattle={
-  minCusPerHour:23,
-  maxCusPerHour:65,
-  avgCookPercus:6.3,
-  cookPerHour:[],
-  randNumOfCus:function(min,max){
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  },
-  calCookPerHour:function()
+let bodyEl=document.getElementById('body');
+let table=document.createElement('table');
+bodyEl.appendChild(table);
+function BranchLocation(location,minCusPerHour,maxCusPerHour,avgCookPercus)
+{
+  this.location=location;
+  this.minCusPerHour=minCusPerHour;
+  this.maxCusPerHour=maxCusPerHour;
+  this.avgCookPercus=avgCookPercus;
+  this.cookPerHour=[];
+  this.dayTotal=0;
+}
+BranchLocation.prototype.randNumOfCus=function()
+{
+  return Math.floor(Math.random() * (this.maxCusPerHour - this.minCusPerHour + 1) + this.minCusPerHour);
+};
+BranchLocation.prototype.calCookPerHour=function()
+{
+  for(let i=0;i<timeArr.length;i++)
   {
-    for(let i=0;i<timeArr.length;i++)
-    {
-      this.cookPerHour.push(Math.floor(this.randNumOfCus(this.minCusPerHour,this.maxCusPerHour)*this.avgCookPercus));
-    }
-  },
-  render:function(){
-    this.calCookPerHour();
-    let bodyEl=document.getElementById('body');
-    let headEl=document.createElement('h2');
-    bodyEl.appendChild(headEl);
-    headEl.textContent='Seattle';
-    let ulEl=document.createElement('ul');
-    bodyEl.appendChild(ulEl);
-    let liEl;
-    let total=0;
-    for(let i=0;i<timeArr.length;i++)
-    {
-      liEl=document.createElement('li');
-      ulEl.appendChild(liEl);
-      liEl.textContent=timeArr[i]+':'+this.cookPerHour[i]+'  '+'cookies';
-      total=total+this.cookPerHour[i];
-    }
-    liEl=document.createElement('li');
-    ulEl.appendChild(liEl);
-    liEl.textContent='Total:'+total+'  '+'cookies';
+    this.cookPerHour.push(Math.floor(this.randNumOfCus()*this.avgCookPercus));
   }
 };
+BranchLocation.prototype.render=function()
+{
+  this.calCookPerHour();
+  let tr=document.createElement('tr');
+  table.appendChild(tr);
+  let th=document.createElement('th');
+  tr.appendChild(th);
+  th.textContent=this.location;
+  let td;
+  for(let i=0;i<this.cookPerHour.length;i++)
+  {
+    td=document.createElement('td');
+    tr.appendChild(td);
+    td.textContent=this.cookPerHour[i];
+    this.dayTotal=this.dayTotal+this.cookPerHour[i];
+  }
+  td=document.createElement('td');
+  tr.appendChild(td);
+  td.textContent=this.dayTotal;
+};
+function headerRow()
+{
+  let tr=document.createElement('tr');
+  table.appendChild(tr);
+  let th=document.createElement('th');
+  tr.appendChild(th);
+  for(let i=0;i<timeArr.length;i++)
+  {
+    th=document.createElement('th');
+    tr.appendChild(th);
+    th.textContent=timeArr[i];
+  }
+  th=document.createElement('th');
+  tr.appendChild(th);
+  th.textContent='Daily Location total';
+}
+function footerRow()
+{
+  let tr=document.createElement('tr');
+  table.appendChild(tr);
+  let th=document.createElement('th');
+  tr.appendChild(th);
+  th.textContent='Total';
+  let td;
+  for(let i=0;i<timeArr.length;i++)
+  {
+    td=document.createElement('td');
+    tr.appendChild(td);
+    td.textContent=seattle.cookPerHour[i]+tokyo.cookPerHour[i]+dubai.cookPerHour[i]+paris.cookPerHour[i]+lima.cookPerHour[i];
+  }
+  td=document.createElement('td');
+  tr.appendChild(td);
+  td.textContent=seattle.dayTotal+tokyo.dayTotal+dubai.dayTotal+paris.dayTotal+lima.dayTotal;
+}
+let seattle=new BranchLocation('Seattle',23,65,6.3);
+let tokyo=new BranchLocation('Tokyo',3,24,1.2);
+let dubai=new BranchLocation('Dubai',11,38,3.7);
+let paris=new BranchLocation('Paris',20,38,2.3);
+let lima=new BranchLocation('Lima',2,16,4.6);
+ 
+headerRow();
 seattle.render();
-
-let tokyo={
-  minCusPerHour:3,
-  maxCusPerHour:24,
-  avgCookPercus:1.2,
-  cookPerHour:[],
-  randNumOfCus:function(min,max){
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  },
-  calCookPerHour:function()
-  {
-    for(let i=0;i<timeArr.length;i++)
-    {
-      this.cookPerHour.push(Math.floor(this.randNumOfCus(this.minCusPerHour,this.maxCusPerHour)*this.avgCookPercus));
-
-    }
-  },
-  render:function(){
-    this.calCookPerHour();
-    let bodyEl=document.getElementById('body');
-    let headEl=document.createElement('h2');
-    bodyEl.appendChild(headEl);
-    headEl.textContent='Tokyo';
-    let ulEl=document.createElement('ul');
-    bodyEl.appendChild(ulEl);
-    let liEl;
-    let total=0;
-    for(let i=0;i<timeArr.length;i++)
-    {
-      liEl=document.createElement('li');
-      ulEl.appendChild(liEl);
-      liEl.textContent=timeArr[i]+':'+this.cookPerHour[i]+'  '+'cookies';
-      total=total+this.cookPerHour[i];
-    }
-    liEl=document.createElement('li');
-    ulEl.appendChild(liEl);
-    liEl.textContent='Total:'+total+'  '+ 'cookies';
-  }
-};
 tokyo.render();
-
-let dubai={
-  minCusPerHour:11,
-  maxCusPerHour:38,
-  avgCookPercus:3.7,
-  cookPerHour:[],
-  randNumOfCus:function(min,max){
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  },
-  calCookPerHour:function()
-  {
-    for(let i=0;i<timeArr.length;i++)
-    {
-      this.cookPerHour.push(Math.floor(this.randNumOfCus(this.minCusPerHour,this.maxCusPerHour)*this.avgCookPercus));
-
-    }
-  },
-  render:function(){
-    this.calCookPerHour();
-    let bodyEl=document.getElementById('body');
-    let headEl=document.createElement('h2');
-    bodyEl.appendChild(headEl);
-    headEl.textContent='Dubai';
-    let ulEl=document.createElement('ul');
-    bodyEl.appendChild(ulEl);
-    let liEl;
-    let total=0;
-    for(let i=0;i<timeArr.length;i++)
-    {
-      liEl=document.createElement('li');
-      ulEl.appendChild(liEl);
-      liEl.textContent=timeArr[i]+':'+this.cookPerHour[i]+'  '+'cookies';
-      total=total+this.cookPerHour[i];
-    }
-    liEl=document.createElement('li');
-    ulEl.appendChild(liEl);
-    liEl.textContent='Total:'+total+'  '+'cookies';
-  }
-};
 dubai.render();
-let paris={
-  minCusPerHour:20,
-  maxCusPerHour:38,
-  avgCookPercus:2.3,
-  cookPerHour:[],
-  randNumOfCus:function(min,max){
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  },
-  calCookPerHour:function()
-  {
-    for(let i=0;i<timeArr.length;i++)
-    {
-      this.cookPerHour.push(Math.floor(this.randNumOfCus(this.minCusPerHour,this.maxCusPerHour)*this.avgCookPercus));
-
-    }
-  },
-  render:function(){
-    this.calCookPerHour();
-    let bodyEl=document.getElementById('body');
-    let headEl=document.createElement('h2');
-    bodyEl.appendChild(headEl);
-    headEl.textContent='Paris';
-    let ulEl=document.createElement('ul');
-    bodyEl.appendChild(ulEl);
-    let liEl;
-    let total=0;
-    for(let i=0;i<timeArr.length;i++)
-    {
-      liEl=document.createElement('li');
-      ulEl.appendChild(liEl);
-      liEl.textContent=timeArr[i]+':'+this.cookPerHour[i]+'   '+'cookies';
-      total=total+this.cookPerHour[i];
-    }
-    liEl=document.createElement('li');
-    ulEl.appendChild(liEl);
-    liEl.textContent='Total:'+total+'  '+'cookies';
-  }
-};
 paris.render();
-let lima={
-  minCusPerHour:2,
-  maxCusPerHour:16,
-  avgCookPercus:4.6,
-  cookPerHour:[],
-  randNumOfCus:function(min,max){
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  },
-  calCookPerHour:function()
-  {
-    for(let i=0;i<timeArr.length;i++)
-    {
-      this.cookPerHour.push(Math.floor(this.randNumOfCus(this.minCusPerHour,this.maxCusPerHour)*this.avgCookPercus));
-
-    }
-  },
-  render:function(){
-    this.calCookPerHour();
-    let bodyEl=document.getElementById('body');
-    let headEl=document.createElement('h2');
-    bodyEl.appendChild(headEl);
-    headEl.textContent='Lima';
-    let ulEl=document.createElement('ul');
-    bodyEl.appendChild(ulEl);
-    let liEl;
-    let total=0;
-    for(let i=0;i<timeArr.length;i++)
-    {
-      liEl=document.createElement('li');
-      ulEl.appendChild(liEl);
-      liEl.textContent=timeArr[i]+':'+this.cookPerHour[i]+'  '+'cookies';
-      total=total+this.cookPerHour[i];
-    }
-    liEl=document.createElement('li');
-    ulEl.appendChild(liEl);
-    liEl.textContent='Total:'+total+'   '+'cookies';
-  }
-};
 lima.render();
-
-
-
-
+footerRow();
 
